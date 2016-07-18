54a042429d630b3a279f41cc37a531176104314c
James <jbkuczma@buffalo.edu>
2016-07-18 13:30:57 -0400

    able to retrieve input from user

---
+++ script.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('#hex').on('input',function(e){
+        var hexData = $('#hex').val();
+    });
+    $('#rgb').on('input',function(e){
+        var rgbData = $('#rgb').val();
+    });
+});
+
+function hexToRGB(hexValue){
+
+}
+function rgbToHex(rgbValue){
+    
+}
