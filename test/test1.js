74f3f93a56d93060774b23c9cda194fdc745ecf1
James <jbkuczma@buffalo.edu>
2016-07-18 10:35:01 -0400

    created basic tests. will change

---
+++ test/test1.js
@@ -0,0 +1,16 @@
+var assert = require('chai').assert;
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(1, [1,2,3].indexOf(2));
+    });
+  });
+  describe('#length', function(){
+     var arr = [];
+     it('should be empty', function(){
+        assert.equal(arr.length, 0);
+     });
+  });
+});
